b560e50781cc9859a2fdc93c0e5d1c92d51914ce
spfscmz <psong4@dons.usfca.edu>
2018-07-17 22:46:52 -0700

    Changed the directory structure

--- Backend_server/service.py
+++ Backend_server/service.py
@@ -2,9 +2,12 @@ from werkzeug.wrappers import Request, Response
 from werkzeug.serving import run_simple
 from jsonrpc import JSONRPCResponseManager, dispatcher
 
+# Documentation see: https://media.readthedocs.org/pdf/json-rpc/latest/json-rpc.pdf
+
 @dispatcher.add_method
 def foobar(**kwargs):
     return kwargs["foo"] + kwargs["bar"]
+
 @Request.application
 def application(request):
 # Dispatcher is dictionary {<method_name>: callable}
@@ -14,5 +17,8 @@ def application(request):
     response = JSONRPCResponseManager.handle(
         request.data, dispatcher)
     return Response(response.json, mimetype='application/json')
+    
 if __name__ == '__main__':
+    print(list(dispatcher.items()))
+    print(Request)
     run_simple('localhost', 4000, application)
